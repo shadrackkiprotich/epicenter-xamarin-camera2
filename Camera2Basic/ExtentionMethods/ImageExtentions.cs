e2c02b9699b817c45ec1f2363a62f2965126c248
Valdas Rakutis <36191877+UndeadRat22@users.noreply.github.com>
2018-11-26 10:55:34 +0200

    Full app.

---
+++ Camera2Basic/ExtentionMethods/ImageExtentions.cs
@@ -0,0 +1,30 @@
+﻿using Android.Media;
+using Java.Nio;
+
+namespace Camera2Basic.ExtentionMethods
+{
+    public static class ImageExtentions
+    {
+        public static byte[] ByteArray(this Image image)
+        {
+            foreach (var plane in image.GetPlanes())
+            {
+                ByteBuffer buffer = plane.Buffer;
+                if (buffer != null)
+                {
+                    byte[] bytes = new byte[buffer.Remaining()];
+                    buffer.Get(bytes);
+                    image.Close();
+                    return bytes;
+                }
+            }
+            return new byte[0];
+        }
+        public static string ToBase64String(this Image image)
+        {
+             if (image == null)
+                return "";
+            return System.Convert.ToBase64String(image.ByteArray());
+        }
+    }
+}
---
+++ Camera2Basic/Services/ImageUploadService.cs
@@ -0,0 +1,31 @@
+﻿using Android.Media;
+using Camera2Basic.ExtentionMethods;
+using RestSharp;
+using Plugin.LocalNotifications;
+
+namespace Camera2Basic.Services
+{
+    public class ImageUploadService
+    {
+        private readonly string ServiceURL = @"https://epicentertop.azurewebsites.net";
+
+        public void Upload(Image image)
+        {
+            string base64 = image.ToBase64String();
+
+            RestClient client = new RestClient(ServiceURL);
+            RestRequest request = new RestRequest("api", Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddBody(base64);
+            client.ExecuteAsync(request, response =>
+            {
+                if (!response.IsSuccessful)
+                {
+                    CrossLocalNotifications.Current.Show("Found something!", "bla bla bla");
+                }
+            });
+        }
+    }
+}
